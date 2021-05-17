59692decdb8095f02fcc2f65c0dd0ce98fcf154b
marcosrdz <marcosrodriguez@me.com>
2021-05-17 12:28:31 -0400

    WIP

--- ios/RnLdk.m
+++ ios/RnLdk.m
@@ -11,6 +11,75 @@ RCT_EXTERN_METHOD(start:(NSString *)entropyHex
 
 RCT_EXTERN_METHOD(getVersion:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 
+RCT_EXTERN_METHOD(getRelevantTxids:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(transactionUnconfirmed:(NSString *)txidHex
+                  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(transactionConfirmed:(NSString *)headerHex
+                  height:(nonnull NSNumber)height
+                  txPos:(nonnull NSNumber)txPos
+                  transactionHex:(NSString *)transactionHex
+                  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+
+RCT_EXTERN_METHOD(updateBestBlock:(NSString *)headerHex
+                  height:(nonnull NSNumber)height
+                  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(connectPeer:(NSString *)pubkeyHex
+                  hostname:(NSString *)pubkeyHex
+                  port:(nonnull NSNumber)port
+                  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+
+RCT_EXTERN_METHOD(sendPayment:(NSString *)destPubkeyHex
+                  paymentHashHex:(NSString *)paymentHashHex
+                  paymentSecretHex:(NSString *)paymentSecretHex
+                  shortChannelId:(NSString *)shortChannelId
+                  paymentValueMsat:(nonnull NSNumber)paymentValueMsat
+                  finalCltvValue:(nonnull NSNumber)finalCltvValue
+                  LdkRoutesJsonArrayString:(NSString *)LdkRoutesJsonArrayString
+                  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(addInvoice:(nonnull NSNumber)amtMsat
+                  description:(NSString *)description
+                  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(listPeers:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+
+RCT_EXTERN_METHOD(getNodeId:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+
+RCT_EXTERN_METHOD(closeChannelCooperatively:(NSString *)channelIdHex
+                  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(closeChannelForce:(NSString *)channelIdHex
+                  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(openChannelStep1:(NSString *)pubkey
+                  channelValue:(nonnull NSNumber)channelValue
+                  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+
+RCT_EXTERN_METHOD(openChannelStep2:(NSString *)txhex
+                  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(listUsableChannels:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+
+RCT_EXTERN_METHOD(listChannels:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+
+RCT_EXTERN_METHOD(setFeerate:(nonnull NSNumber)newFeerateFast
+                  newFeerateMedium:(nonnull NSNumber)newFeerateMedium
+                  newFeerateSlow:(nonnull NSNumber)newFeerateSlow
+                  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+
+RCT_EXTERN_METHOD(fireAnEvent:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
 
 
 @end
--- ios/RnLdk.swift
+++ ios/RnLdk.swift
@@ -12,4 +12,100 @@ class RnLdk: NSObject {
     func getVersion(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
         resolve("0.0.18")
     }
+    
+    func getName() -> String {
+        return "RnLdk"
+    }
+    
+    @objc
+    func getRelevantTxids(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    @objc
+    func transactionUnconfirmed(txidHex: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+    }
+    
+    @objc
+    func transactionConfirmed(headerHex: String, height: Int, txPos: Int, transactionHex: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    @objc
+    func updateBestBlock(headerHex: String, height: Int, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+    }
+    
+    @objc
+    func connectPeer(pubkeyHex: String, hostname: String, port: Int, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    @objc
+    func sendPayment(destPubkeyHex: String, paymentHashHex: String, paymentSecretHex: String, shortChannelId: String, paymentValueMsat: Int, finalCltvValue: Int, LdkRoutesJsonArrayString: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+    }
+    
+    @objc
+    func addInvoice(amtMsat: Int, description: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    @objc
+    func listPeers(resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    private func handleEvent(event: Event) {
+        
+    }
+    
+    @objc
+    func getNodeId(resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    @objc
+    func getChannelManagerBytes(resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    @objc
+    func closeChannelCooperatively(channelIdHex: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    @objc
+    func closeChannelForce(channelIdHex: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    @objc
+    func openChannelStep1(pubkey: String, channelValue: Int, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    @objc
+    func openChannelStep2(txhex: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    @objc
+    func listUsableChannels(resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    @objc
+    func listChannels(resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    @objc
+    func setFeerate(newFeerateFast: Int, newFeerateMedium: Int, newFeerateSlow: Int, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    @objc
+    func fireAnEvent(resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseResolveBlock) {
+        
+    }
+    
+    
 }
