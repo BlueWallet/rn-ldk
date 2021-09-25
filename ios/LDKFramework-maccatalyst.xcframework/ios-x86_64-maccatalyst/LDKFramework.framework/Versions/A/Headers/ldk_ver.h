7b855c40ddf840731dbb425a84643fbcd1b443ad
Marcos Rodriguez Vélez <marcosrodriguez@me.com>
2021-09-25 14:09:44 -0400

    Update frameworks

--- ios/LDKFramework-maccatalyst.xcframework/ios-x86_64-maccatalyst/LDKFramework.framework/Versions/A/Headers/ldk_ver.h
+++ ios/LDKFramework-maccatalyst.xcframework/ios-x86_64-maccatalyst/LDKFramework.framework/Versions/A/Headers/ldk_ver.h
@@ -8,8 +8,8 @@ static inline int _ldk_strncmp(const char *s1, const char *s2, uint64_t n) {
 	return 0;
 }
 
-#define _LDK_HEADER_VER "v0.0.100-1-gdba15b73610e4f54"
-#define _LDK_C_BINDINGS_HEADER_VER "v0.0.100.1-dirty"
+#define _LDK_HEADER_VER "v0.0.101-2-ge9c4fa6c6959f2cd"
+#define _LDK_C_BINDINGS_HEADER_VER "v0.0.100.0-30-gd3efae5be843d88e"
 static inline const char* check_get_ldk_version() {
 	LDKStr bin_ver = _ldk_get_compiled_version();
 	if (_ldk_strncmp(_LDK_HEADER_VER, (const char*)bin_ver.chars, bin_ver.len) != 0) {
