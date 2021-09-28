a51d213f66946f80e0ce2866de90090589c79520
Overtorment <overtorment@gmail.com>
2021-09-28 16:52:31 +0100

    REL

--- android/src/main/java/com/rnldk/RnLdkModule.kt
+++ android/src/main/java/com/rnldk/RnLdkModule.kt
@@ -55,7 +55,7 @@ class RnLdkModule(private val reactContext: ReactApplicationContext) : ReactCont
 
   @ReactMethod
   fun getVersion(promise: Promise) {
-    promise.resolve("0.0.101.1");
+    promise.resolve("0.0.101.2");
   }
 
   @ReactMethod
