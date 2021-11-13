b642da6b0476d1a539c300466b148772a1155e1a
Overtorment <overtorment@gmail.com>
2021-11-13 11:11:50 +0000

    REF

--- android/src/main/java/com/rnldk/RnLdkModule.kt
+++ android/src/main/java/com/rnldk/RnLdkModule.kt
@@ -268,7 +268,7 @@ class RnLdkModule(private val reactContext: ReactApplicationContext) : ReactCont
   fun connectPeer(pubkeyHex: String, hostname: String, port: Int, promise: Promise) {
     println("ReactNativeLDK: connecting to peer " + pubkeyHex);
     try {
-      nio_peer_handler?.connect(hexStringToByteArray(pubkeyHex), InetSocketAddress(hostname, port), 9000);
+      nio_peer_handler!!.connect(hexStringToByteArray(pubkeyHex), InetSocketAddress(hostname, port), 9000);
       promise.resolve(true)
     } catch (e: IOException) {
       promise.reject("connectPeer exception: " + e.message);
